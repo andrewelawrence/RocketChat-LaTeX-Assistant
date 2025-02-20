be89d9ff7fdfd8446f77e7906b0b342d3f664832
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2025-02-20 01:50:38 -0500

    update test

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 import requests
 
-response_main = requests.get("https://operational-missie-tufts-cs0150-04-spr25-76ae7ad6.koyeb.app/query")
-print('Web Application Response:\n', response_main.text, '\n\n')
-
-
-data = {"text":"tell me about tufts"}
+data = {"text":"how can I make a centered table with 3 columns?"}
 response_llmproxy = requests.post("https://operational-missie-tufts-cs0150-04-spr25-76ae7ad6.koyeb.app/query", json=data)
 print('LLMProxy Response:\n', response_llmproxy.text)
