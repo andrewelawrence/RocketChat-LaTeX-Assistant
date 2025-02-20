32b865576fe18bff1d48447a7317ac16fb7a1283
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2025-02-20 01:50:30 -0500

    add RAG + system prompt
    Chatbot purpose - specific LaTeX documentation assistence.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return jsonify({"text":'Hello from Koyeb - you reached the main page!'})
+   return jsonify({"text":'Hi. There is nothing on this page - please return from where you came from!'})
 
 @app.route('/query', methods=['POST'])
 def main():
@@ -26,12 +26,26 @@ def main():
 
     # Generate a response using LLMProxy
     response = generate(
-        model='4o-mini',
-        system='answer my question and add keywords',
+        model='anthropic.claude-3-haiku-20240307-v1:0',
+        system='''
+        You are a LaTeX/TeX programming assistant. 
+        Respond only to LaTeX-related questions, providing answers backed by specific passages from the following documents:
+            LaTeX For Authors - Current Version.pdf,
+            LaTeX For Authors - Historic Version.pdf,
+            LaTeX Package and Class Authors - Current Version.pdf,
+            Package AMSMATH - User’s Guide for the amsmath Package (Version 2.1).pdf,
+            Package BOOKTABS - Publication quality tables in LaTeX.pdf,
+            Package GRAPHICS - Packages in the ‘graphics’ bundle.pdf.
+        Use RAG to cite relevant excerpts from these documents whenever possible. 
+        If a relevant answer is not found in the RAG sources, state clearly that the information is unavailable from the provided documents, and then provide a non-RAG response if necessary.
+        Be restrictive to LaTeX content only. Keep responses focused, accurate, and concise. ''',
         query= message,
-        temperature=0.0,
+        temperature=0.3,
         lastk=0,
-        session_id='GenericSession'
+        rag_usage=True,
+        rag_k=999,
+        rag_threshold=0.5,
+        session_id='hw04',
     )
     print(response)
     response_text = response['response']
