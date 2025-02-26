3885434c56c006ff0764fca6372d6ef9f5ccae20
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2025-02-25 19:06:46 -0500

    add logging

--- add_context.py
+++ add_context.py
@@ -1,14 +1,9 @@
 import sys, os
 from llmproxy import pdf_upload
+from logger_config import get_logger
 
-'''
-Reminder, export environment variables prior to running this function.
-```bash
-$ export apiKey="x"
-$ export endPoint="y"
-
-Useful resource: https://ctan.org/tex-archive
-'''
+_LOGGER = get_logger(__name__)
+_LOGGER.info("Add Context Begun.")
 
 if len(sys.argv) < 2:
     print("Usage: python script.py <pdf filepath> [<pdf filepath>]")
@@ -19,4 +14,4 @@ for file_path in sys.argv[1:]:
     response = pdf_upload(path = file_path,
                           description = desc,
                           session_id = 'hw04')
-    print(f"Response for {desc}: {response}")
+    _LOGGER.info(f"Response for {desc}: {response}")
