a303669ba889979db0d5f4c2984a95006b020565
Andrew Lawrence <andrew.e.lawrence@gmail.com>
2025-09-28 23:28:28 -0400

    add_context: switch to config.get_logger; clarify CLI usage; mark static session_id as TODO

--- add_context.py
+++ add_context.py
@@ -1,17 +1,17 @@
 import sys, os
 from llmproxy import pdf_upload
-from logger_config import get_logger
+from config import get_logger
 
 _LOGGER = get_logger(__name__)
 _LOGGER.info("Add Context Begun.")
 
 if len(sys.argv) < 2:
-    print("Usage: python script.py <pdf filepath> [<pdf filepath>]")
+    print("Usage: python add_context.py <pdf filepath> [<pdf filepath>]")
     sys.exit(1)
 
 for file_path in sys.argv[1:]:
     desc = os.path.basename(file_path)
     response = pdf_upload(path = file_path,
                           description = desc,
-                          session_id = 'hw04')
+                          session_id = 'hw04') # TODO: fix this
     _LOGGER.info(f"Response for {desc}: {response}")
